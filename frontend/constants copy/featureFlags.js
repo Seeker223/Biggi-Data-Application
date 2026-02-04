251926112ad732475ec0d107ad1f185f20b8dd19
BADUN OLUWATOBI <badunoluwatobi2024@gmail.com>
2026-02-04 15:44:00 +0100

    updted

--- frontend/constants copy/featureFlags.js
+++
@@ -1,6 +0,0 @@
-// Central feature flags for temporarily disabling risky features during review
-export const FEATURE_FLAGS = {
-  DISABLE_GAME_AND_REDEEM: process.env.EXPO_PUBLIC_DISABLE_GAME_AND_REDEEM === "true" || false, // set to `true` to disable game/redeem flows
-};
-
-export default FEATURE_FLAGS;
--- frontend/constants copy/font.js
+++
@@ -1,5 +0,0 @@
-export const fontMap = {
-  "Red Rose": require("../assets/fonts/RedRose-Bold.ttf"),
-  "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-  "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-};
